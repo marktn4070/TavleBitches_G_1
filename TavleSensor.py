c55cb5be8ae07dcf9a6df66a60cd2c82d7822aa4
Hemold <maltelauritsen2@gmail.com>
2023-10-12 11:47:03 +0200

    kkk

--- TavleSensor.py
+++ TavleSensor.py
@@ -1,23 +1,17 @@
 import RPi.GPIO as GPIO
 
-linefollower1 = 9
-linefollower2 = 10
+linefollower1 = 24
+linefollower2 = 26
 
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(linefollower1,GPIO.IN)
+try:
+   while True:
+    Venstre = int (GPIO.input(linefollower1))
+    print(Venstre)
+except KeyboardInterrupt:
+  pass
 GPIO.cleanup()
-
-while True:
-   GPIO.setup(linefollower1,GPIO.OUT)
-   GPIO.output(linefollower1,True)
-   sleep(0.00001)
-   GPIO.output(linefollower1,False)
-   GPIO.setup(linefollower1,GPIO.IN)
-   timest = time.time_ns()
-   while (GPIO.input(linefollower1)==True and ((time.time_ns()) - timest < 3000000)):
-      timeCalc = (time.time_ns()) - timest 
-      print(timeCalc)
-      sleep(0.25)
-
-   
