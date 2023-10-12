803d95f138abe4dad25c208d7d768457e59b90cf
Mark <snapstil1eren@gmail.com>
2023-10-12 11:21:34 +0200

    efdvfe

--- TavleBitches_Gruppe_1.py
+++ TavleBitches_Gruppe_1.py
@@ -1,10 +1,13 @@
+#sensor test
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+
+
+
+
+
 #PWM test
-
-
-
-
-
-
 import RPi.GPIO as GPIO
 from time import sleep
 
@@ -55,3 +58,32 @@ while True:
         pi_pwm.ChangeDutyCycle(duty)
         pi_pwm1.ChangeDutyCycle(duty)
         sleep(0.1)
+
+
+
+        #______________Sensor_start_____________
+A = 2
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+while True:
+   GPIO.setup(A,GPIO.OUT)
+   GPIO.output(A,True)
+   sleep(0.00001)
+   GPIO.output(A,False)
+   GPIO.setup(A,GPIO.IN)
+   timest = time.time_ns()
+   while (GPIO.input(A)==True and ((time.time_ns()) - timest < 3000000)):
+      timeCalc = (time.time_ns()) - timest 
+      print(timeCalc)
+      sleep(0.25)
+   
+
+
+
+
+
+
+        #______________Sensor_slut______________
