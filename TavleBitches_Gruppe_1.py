fb6f1330293e460cb11ce725706f989a7067ac16
Hemold <maltelauritsen2@gmail.com>
2023-10-12 12:28:51 +0200

    uiuu

--- TavleBitches_Gruppe_1.py
+++ TavleBitches_Gruppe_1.py
@@ -54,3 +54,26 @@ while True:
         pi_pwm1.ChangeDutyCycle(duty)
         sleep(0.1)
 
+
+
+
+
+linefollower1 = 23
+linefollower2 = 24
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(linefollower1,GPIO.IN)
+GPIO.setup(linefollower2,GPIO.IN)
+try:
+   while True:
+    Venstre = int (GPIO.input(linefollower1))
+    print(Venstre)
+    Højre = int (GPIO.input(linefollower2))
+    print(Højre)
+    time.sleep(0.1)
+except KeyboardInterrupt:
+  pass
+GPIO.cleanup()
