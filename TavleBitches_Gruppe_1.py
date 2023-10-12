705872750bdb5afc2df6f61fe324077076ecc2ff
Mark <snapstil1eren@gmail.com>
2023-10-12 12:23:45 +0200

    dewd

--- TavleBitches_Gruppe_1.py
+++ TavleBitches_Gruppe_1.py
@@ -54,34 +54,3 @@ while True:
         pi_pwm1.ChangeDutyCycle(duty)
         sleep(0.1)
 
-
-
-        #______________Sensor_start_____________
-linefollower1 = 9
-linefollower2 = 10
-
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.cleanup()
-
-while True:
-   GPIO.setup(linefollower1,GPIO.OUT)
-   GPIO.output(linefollower1,True)
-   sleep(0.00001)
-   GPIO.output(linefollower1,False)
-   GPIO.setup(linefollower1,GPIO.IN)
-   timest = time.time_ns()
-   while (GPIO.input(linefollower1)==True and ((time.time_ns()) - timest < 3000000)):
-      timeCalc = (time.time_ns()) - timest 
-      print(timeCalc)
-      sleep(0.25)
-
-   
-
-
-
-
-
-
-        #______________Sensor_slut______________
