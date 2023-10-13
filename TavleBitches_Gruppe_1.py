f97c7a550877df5cda0d47b0381bcee478c21817
PloxyP <mattias.bonnesen@gmail.com>
2023-10-13 10:31:40 +0200

    sf

--- TavleBitches_Gruppe_1.py
+++ TavleBitches_Gruppe_1.py
@@ -42,8 +42,8 @@ def koer():
     GPIO.output(DirectionPin2, True)
     GPIO.output(DirectionPin3, True)
     
-    pi_pwm.ChangeDutyCycle(70) #45
-    pi_pwm1.ChangeDutyCycle(80) #50
+    pi_pwm.ChangeDutyCycle(70) #45 #70
+    pi_pwm1.ChangeDutyCycle(80) #50 #80
     #sleep(0.1)
 
     #start PWM of required Duty Cycle. 
@@ -67,7 +67,7 @@ def dven():
     GPIO.output(DirectionPin2, True)
     GPIO.output(DirectionPin3, True)
 
-    pi_pwm.ChangeDutyCycle(80) #50
+    pi_pwm.ChangeDutyCycle(60) #50
     pi_pwm1.ChangeDutyCycle(100) #50
     #sleep(0.05)
 
@@ -81,7 +81,7 @@ def dhoej():
     GPIO.output(DirectionPin3, False)
 
     pi_pwm.ChangeDutyCycle(100) #50
-    pi_pwm1.ChangeDutyCycle(80) #50
+    pi_pwm1.ChangeDutyCycle(60) #50
     #sleep(0.05)
 
 
